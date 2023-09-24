cd4e666aa5bb9c8d389f5026966a92ba12ee2deb
Natalia Cyrklaff <natkapietruszki2000@gmail.com>
2023-09-24 14:49:47 +0200

    Added class for player end blueprint

--- Source/dungeonCrawlerMgr/PlayerEnd.cpp
+++ Source/dungeonCrawlerMgr/PlayerEnd.cpp
@@ -1,2 +1,17 @@
 #include "PlayerEnd.h"
 
+APlayerEnd::APlayerEnd()
+{
+	DefaultSceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceneRoot"));
+	RootComponent = DefaultSceneRoot;
+	fx = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("fx"));
+	fx->SetupAttachment(RootComponent);
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticleAsset(TEXT("ParticleSystem'/Game/FXVarietyPack/Particles/P_ky_magicCircle1.P_ky_magicCircle1'"));
+	if (ParticleAsset.Succeeded())
+	{
+		fx->SetTemplate(ParticleAsset.Object);
+		FLinearColor color = FLinearColor(0.0f, 2.393682f, 15.0f, 0.0f);
+		FVector colorAsVector = FVector(color.R, color.G, color.B);
+		fx->SetVectorParameter("color", colorAsVector);
+	}
+}
--- Source/dungeonCrawlerMgr/PlayerEnd.h
+++ Source/dungeonCrawlerMgr/PlayerEnd.h
@@ -2,11 +2,19 @@
 
 #include "CoreMinimal.h"
 #include "PortalBase.h"
+#include "Particles/ParticleSystemComponent.h"
 #include "PlayerEnd.generated.h"
 
 UCLASS()
 class DUNGEONCRAWLERMGR_API APlayerEnd : public APortalBase
 {
 	GENERATED_BODY()
-	
+public:
+	APlayerEnd();
+
+	UPROPERTY(VisibleAnywhere)
+		USceneComponent* DefaultSceneRoot;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Effects")
+		UParticleSystemComponent* fx;
 };
