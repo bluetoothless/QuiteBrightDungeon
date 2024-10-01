cc2516f3530b337fb24ddd9274e3bcaa26ff2d3f
Natalia Cyrklaff <natkapietruszki2000@gmail.com>
2024-10-01 23:47:53 +0200

    Changed dirs

--- Installation/Installation.bat
+++ Installation/Installation.bat
@@ -1,43 +1,89 @@
 @echo off
 
-REM Set the name of the Conda environment
-set env_name=pcgrl_env
-set env_path=D:\Anaconda3\envs\pcgrl_env
+REM Set the Anaconda directory and virtual environment name
+set python_dir_for_conda=C:\Python39
+set conda_dir=D:\Anaconda3
+set conda_env_name=CondaEnv
 
-ECHO Preparing Conda Environment...
+REM Set the Python directory and virtual environment name
+set python_dir=C:\Python311
+set venv_project_path=VirtualEnv
 
-REM Remove existing Conda environment if it exists
-call conda env remove --name %env_name% --yes
+ECHO Installing Conda environment...
 
-REM Create a new Conda environment with specified Python version
-call conda create --prefix %env_path% python=3.7.0 --yes
+REM Check if Anaconda is installed
+if not exist "%conda_dir%" (
+    echo Anaconda is not installed at %conda_dir%. Please install it first.
+    goto :eof
+)
 
-REM Activate the Conda environment
-CALL conda activate %env_path%
+REM Check if Python is installed
+if not exist "%python_dir%" (
+    echo Installing python to %python_dir%
+    .\python-3.9.1.exe /Include_test=0 TargetDir=%python_dir% /Include_debug=1
+    if errorlevel 1 (
+        echo Installation failed.
+        exit /b 1
+    ) else (
+        echo Installation completed successfully.
+    )
+) else (
+    echo Python is already installed at %python_dir%.
+)
 
-REM Install CUDA Toolkit
-call conda install -c conda-forge cudatoolkit=10.0 --yes
+REM Check if the conda environment exists, if not, create it and install the required packages
+call %conda_dir%\Scripts\conda info --envs | findstr /B /C:"%conda_env_name%"
+if errorlevel 1 (
+    echo Creating Anaconda environment
+    call %conda_dir%\Scripts\conda create --name %conda_env_name% python=3.9 -y
 
-REM Uninstall existing TensorFlow packages if they are installed
-call conda uninstall tensorflow tensorflow-estimator tensorboard --yes
+  echo Activating the Anaconda environment
+  call %conda_dir%\Scripts\conda activate %conda_env_name%
 
-REM Install specific versions of TensorFlow packages
-call conda install tensorflow==1.15.0 tensorflow-estimator==1.15.1 tensorflow-gpu==1.15.0 tensorboard==1.15.0 --yes
+    echo Installing requirements
+    call %conda_dir%\Scripts\conda install --file training_env_requirements.txt -y
 
-REM Install other required packages with pip
-call pip install stable-baselines==2.9.0
-call pip install gym==0.14.0
-call pip install tensorflow-gpu==1.15
+    REM Installing PyTorch with CUDA 11.1 support (or closest available)
+    call %conda_dir%\Scripts\conda install pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c conda-forge -y
+  
+  echo Deactivating the Anaconda environment
+  call %conda_dir%\Scripts\conda deactivate
+)
 
-REM Install OpenAI
-call pip install openai
+ECHO Installing virtual environment...
 
-REM Install current directory as a package
-call pip install -e .
+REM Check if Python is installed
+if not exist "%python_dir%" (
+    echo Installing python to %python_dir%
+    .\python-3.11.1.exe /Include_test=0 TargetDir=%python_dir% /Include_debug=1
+    if errorlevel 1 (
+        echo Installation failed.
+        exit /b 1
+    ) else (
+        echo Installation completed successfully.
+    )
+) else (
+    echo Python is already installed at %python_dir%.
+)
 
-REM Set environment variables
-set CUDA_HOME=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.0
-set PATH=%CUDA_HOME%\bin;%CUDA_HOME%\libnvvp;%PATH%
 
-ECHO Environment setup complete!
-pause
+REM Check if virtualenv is installed
+if not exist "%python_dir%\Scripts\virtualenv.exe" (
+    echo Installing virtualenv
+    %python_dir%\Scripts\pip -q install virtualenv --disable-pip-version-check
+)
+
+REM Check if the virtual environment exists, if not, create it and install the required packages
+if not exist "%venv_project_path%\Scripts\python.exe" (
+    echo Creating python virtual environment
+    %python_dir%\python -m venv %venv_project_path%
+
+  echo Upgrading pip in the virtual environment
+  %venv_project_path%\Scripts\python.exe -m pip install --upgrade pip --disable-pip-version-check
+    REM %venv_project_path%\Scripts\pip install --upgrade pip --disable-pip-version-check
+  
+    echo Installing requirements
+    %venv_project_path%\Scripts\pip install --disable-pip-version-check -q -r requirements.txt
+)
+
+echo on
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# QuiteBrightDungeon
- 
+# Quite Bright Dungeon
+### Instalacja
+1. W celu korzystania z opcji generacji poziomów innych niż "Default map (no generation)", pobierz i zainstaluj Anaconda3 w ścieżce "D:/Anaconda3" https://www.anaconda.com/download, a następnie uruchom plik instalacyjny ./dungeonCrawlerMgr/Installation/Installation.bat.
+2. Korzystanie z dużego modelu językowego (LLM) jako metody generacji poziomów może wymagać uruchomienia dowolnego VPNa, wybierając jedną z lokalizacji, w których dostępne jest API Gemini: https://ai.google.dev/gemini-api/docs/available-regions?hl=pl.
+
+### Sterowanie
+|Klawisze               |Akcja                                |
+|-----------------------|-------------------------------------|
+|W + poruszanie myszą   |ruch postaci w kierunku kursora myszy|
+|Podwójne kliknięcie LPM|uderzenie mieczem                    |
+|Spacja                 |zmiana widoku kamery [postać/mapa]   |
+|ESC                    |wyjście z gry                        |
